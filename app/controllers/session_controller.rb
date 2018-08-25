601c326e4140eebf5be578e093dacac2d26012c5
Jacilyn Hayden <jacihayden@gmail.com>
2018-08-25 20:33:14 +0000

    automatically backed up by learn

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,10 +1,12 @@
 class SessionController < ApplicationController
-    def create
+  def create
     @auth = request.env['omniauth.auth']
     @user = User.find_or_create_by(uid: @auth['uid']) do |u|
       u.name = @auth['info']['name']
       u.email = @auth['info']['email']
     end
+    
     session[:user_id] = @user.id
   end
+  
 end
